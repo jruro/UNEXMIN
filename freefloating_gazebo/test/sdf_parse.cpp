1ee4723ab53f990092f58cce5fb8253dd16af4a1
jruro <33218956+jruro@users.noreply.github.com>
2018-12-19 18:37:56 +0100

    Add files via upload

---
+++ freefloating_gazebo/test/sdf_parse.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <freefloating_gazebo/hydro_model_parser.h>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+ros::init(argc, argv, "sdf_parse");
+ros::NodeHandle nh;
+
+ffg::HydroModelParser parser;
+parser.parseAll(nh);
+auto map = parser.thrusterMap();
+std::cout << "Map: " << map << std::endl;
+}
